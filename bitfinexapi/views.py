f4cdb5fdb96013d3b7894382cf43912b8f692dd3
Serj <serj.dukareff@gmail.com>
2019-06-24 00:51:44 +0300

    Raise 404 if rate not found

--- bitfinexapi/views.py
+++ bitfinexapi/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -11,11 +12,14 @@ class RateView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request, format=None, cid=None):
-        last_rates = Rate.objects.filter(currency_id=cid)\
-            .order_by('-date')[:10]
-        
-        return Response({
-            'currency' : last_rates[0].currency.name,
-            'rate' : last_rates[0].rate,
-            'volume' : sum(r.volume for r in last_rates) / len(last_rates)
-        })
+        try:
+            last_rates = Rate.objects.filter(currency_id=cid)\
+                .order_by('-date')[:10]
+            
+            return Response({
+                'currency' : last_rates[0].currency.name,
+                'rate' : last_rates[0].rate,
+                'volume' : sum(r.volume for r in last_rates) / len(last_rates)
+            })
+        except Exception:
+            raise Http404
